3602df3f308ad0fdd9b94774087daf324b27aece
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-05-26 13:26:45 +0800

    error popover is made

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import errorStyles from "../styles/errorPopover.module.css";
 
 function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [data, setData] = useState();
+    const [error, setError] = useState(null);
 
     const submit = async (e) => {
         e.preventDefault();
@@ -14,20 +16,50 @@ function Login() {
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username: username, password: password }),
             });
-            console.log(response);
             if (!response.ok) {
                 throw new Error("responese is not ok");
             }
             const data = await response.json();
             console.log(data);
         } catch (error) {
+            setError(error);
             console.trace(error);
         }
     };
 
+    const popoverRef = useRef(null);
+
+    useEffect(() => {
+        const popover = popoverRef.current;
+        if (!popover) {
+            return;
+        }
+        if (error) {
+            popover.classList.add(errorStyles.opened);
+            popover.classList.remove(errorStyles.closed);
+        } else {
+            popover.classList.remove(errorStyles.opened);
+            popover.classList.add(errorStyles.closed);
+        }
+    }, [error]);
+
+    /**
+     *
+     * @param {String} error Error string to render
+     * @returns html
+     */
+    function ErrorPopover({ error }) {
+        return (
+            <div ref={popoverRef} className={errorStyles.popover}>
+                <h1>Error</h1>
+                <p>{error}</p>
+            </div>
+        );
+    }
     return (
         <>
             <h1>Login</h1>
+            <ErrorPopover error="error text" />
             <form>
                 <label htmlFor="username">Username: </label>
                 <input
