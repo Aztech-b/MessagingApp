99248758c6d5340bcf9da3214e6dff92fec04ff0
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-05-30 21:02:59 +0800

    frontend: login popover is fixed

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import styles from "../styles/sidebar.module.css";
 import profileLogo from "../assets/user.svg";
 import { useState } from "react";
 import { Link } from "react-router";
+import { Button } from "@mantine/core";
 
 function Sidebar() {
     return (
@@ -12,43 +13,31 @@ function Sidebar() {
     );
 }
 
-/**
- *
- * @param {icon} icon
- * @returns jsx
- */
+function LoginButton() {
+    return (
+        <Link to="/login">
+            <Button variant="filled" color="accent.3">
+                Login
+            </Button>
+        </Link>
+    );
+}
 
 function Account({ children }) {
     const { user } = useUserContext();
     const [isOpen, setIsOpen] = useState(false);
 
-    function AccountMenu({ children }) {
-        return <>{isOpen && children}</>;
-    }
-    function AccountInfo() {
-        return (
-            <div className={styles.menu}>
-                <div className={styles.info}>
-                    <p>{user ? user.username : <Link to="/login">Login</Link>}</p>
-                </div>
-            </div>
-        );
-    }
-
     return (
         <>
             <div className={styles.item}>
-                <div
-                    className={styles.account}
-                    onClick={() => {
-                        setIsOpen(!isOpen);
-                    }}
-                >
+                <button className={styles.account} popoverTarget="menu">
                     <img src={profileLogo} />
+                </button>
+                <div className={styles.menu} popover="auto" id="menu">
+                    <div className={styles.info}>
+                        <p>{user ? user.username : <LoginButton />}</p>
+                    </div>
                 </div>
-                <AccountMenu>
-                    <AccountInfo></AccountInfo>
-                </AccountMenu>
             </div>
         </>
     );
