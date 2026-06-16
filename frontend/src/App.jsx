5d6962cafcd7975b8a3639c7addb9ee34b719ecf
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-06-17 04:52:52 +0800

    fixed redirect to landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { Outlet, useLocation, useNavigate } from "react-router";
 import useUser from "./hooks/useUser.jsx";
 import { UserDataProvider } from "./components/Context";
+import { useEffect } from "react";
 
 function App() {
     const { user } = useUser();
-    const location = useLocation();
     const navigate = useNavigate();
-    if (location.pathname === "/") {
-        navigate("/landing-page");
-    }
+    const location = useLocation();
+    useEffect(() => {
+        if (location.pathname === "/") {
+            navigate("/landing-page");
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location.pathname]);
     return (
         <>
             <UserDataProvider value={{ user }}>
