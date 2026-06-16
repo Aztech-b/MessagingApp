80961bcb6500dcc4e6cc448b5dfd3ad8d4bfc38c
Bakdaulet <bakdaulet.azkhan@proton.me>
2026-06-17 04:40:53 +0800

    added redirect if on '/' address

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import useUser from "./hooks/useUser.jsx";
 import { UserDataProvider } from "./components/Context";
 
 function App() {
     const { user } = useUser();
-
+    const location = useLocation();
+    const navigate = useNavigate();
+    if (location.pathname === "/") {
+        navigate("/landing-page");
+    }
     return (
         <>
             <UserDataProvider value={{ user }}>
